31c9fdcddb82e479d2de907a889e9ddbb0a4a4dc
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-02-07 22:47:50 -0300

    criação conteúdo card home

--- School-ADM/src/layouts/HomeClassCard.tsx
+++ School-ADM/src/layouts/HomeClassCard.tsx
@@ -1,4 +1,4 @@
-import { ClassCard, LenguageFlag, ClassName, CourseLevel, CardInfo, Information, ItemIcon, ItemDescription } from "./HomeClassCardStyle";
+import { ClassCard, LenguageFlag, ClassName, CourseLevel, CardRow, Information, TextContent, ItemIcon, ItemDescription, CardInfo } from "./HomeClassCardStyle";
 import deutschland from '../assets/images/deutschland_flagge.png';
 import frankreich from '../assets/images/frankreich_flagge.png';
 import usa from '../assets/images/usa_flagge.png';
@@ -8,34 +8,35 @@ import italien from '../assets/images/italien_flagge.png';
 
 function HomeClassCard(){
 
+    const cardIcons = ["fab fa-instagram", "fab fa-facebook", "fab fa-whatsapp", "fab fa-envelope"];
+
     return(
         <>
 
             <ClassCard>
+
                 <LenguageFlag src={deutschland} />
-                <ClassName> Alemão - B1.2</ClassName>
-                <CourseLevel> B1.2 </CourseLevel>
+
+                <CardRow>
+
                 <CardInfo>
-                    <Information>
-                        <ItemIcon> efvefv</ItemIcon>
-                        <ItemDescription> vfvwvwf</ItemDescription>
-                    </Information>
-
-                    <Information>
-                        <ItemIcon> efvefv</ItemIcon>
-                        <ItemDescription> vfvwvwf</ItemDescription>
-                    </Information>
-
-                    <Information>
-                        <ItemIcon> efvefv</ItemIcon>
-                        <ItemDescription> vfvwvwf</ItemDescription>
-                    </Information>
-
-                    <Information>
-                        <ItemIcon> efvefv</ItemIcon>
-                        <ItemDescription> vfvwvwf</ItemDescription>
-                    </Information>
+                    <ClassName> Alemão - B1.2</ClassName>
+                    <CourseLevel> B1.2 </CourseLevel>
                 </CardInfo>
+
+                <TextContent>
+
+                    {cardIcons.map((item) => 
+                        <Information>
+                            <ItemIcon className={item} />
+                            <ItemDescription> vfvwvwf</ItemDescription>
+                        </Information>
+                    )}
+
+                </TextContent>
+
+
+                </CardRow>
 
             </ClassCard>
 
@@ -76,4 +77,13 @@ function HomeClassCard(){
 }
 
 export default HomeClassCard;
+
+/*
+
+                
+                
+                
+
+
+*/
 
--- School-ADM/src/layouts/HomeClassCardStyle.ts
+++ School-ADM/src/layouts/HomeClassCardStyle.ts
@@ -9,6 +9,13 @@ export const ClassCard = style.div`
     border: solid 2px #DEDEDE;
     border-radius: 15px;
     overflow: hidden;
+    transition: transform 0.2s;
+    cursor: pointer;
+
+    &:hover{
+        cursor pointer;
+        transform: translate(0, -5%);
+    }
 `;
 
 export const LenguageFlag = style.img`
@@ -20,17 +27,15 @@ export const ClassName = style.div`
     display: flex;
     flex-warp: warp;
     font-family: verdana;
-
     font-size: 140%;
     padding: 3%;
     color: #333333;
-
 `;
 
 export const CourseLevel = style.div`
     font-family: verdana;
     margin-left: auto;
-    height: 16%;
+    height: 60%;
     border-bottom-left-radius: 15px;
     font-size: 120%;
     padding: 3%;
@@ -39,17 +44,39 @@ export const CourseLevel = style.div`
 `;
 
 export const CardInfo = style.div`
-
+    display: flex;
 `;
 
 export const Information = style.div`
-
+    display: flex;
+    width: 50%;    
+    gap: 5%;
 `;
 
-export const ItemIcon = style.div`
+export const ItemIcon = style.i`
+    display: flex;
+    align-self: center;
+`;
 
+export const TextContent = style.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+    padding: 2%;
 `;
 
 export const ItemDescription = style.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-self: center;
+    font-style: italic;
+`;
 
+export const CardRow = style.div`
+    display: flex;
+    flex-direction: column;
+    width: 85%;
+    height: 100%;
 `;
