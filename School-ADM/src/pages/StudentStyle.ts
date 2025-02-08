2930202f407f03181e22eaeea698487b6a151c36
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-02-08 11:59:37 -0300

    criação componente search-bar

---
+++ School-ADM/src/pages/StudentStyle.ts
@@ -0,0 +1,52 @@
+import style from 'styled-components';
+
+
+export const SearchBar = style.div`
+    display: flex;
+    width: 45%;
+    height: 6%;
+    border: solid 2px #DEDEDE;
+    border-radius: 50px;
+    overflow: hidden;
+    transition: transform 0.2s;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #FAF5F5;
+    }
+
+`;
+
+export const SearchField = style.input`
+    border: none;
+    flex: 1;
+    width: 95%;
+    font-size: 15px;
+    margin: 0% 4%;
+    outline: none;
+    border: none;
+    box-shadow: none;
+    background: none
+`;
+
+export const IconContainer = style.div`
+    display: flex;
+    width: 12%;
+    height: 100%;
+    border-left: solid 2px  #DEDEDE;
+    justify-content: center;
+
+    &:hover {
+        background-color: #b1aeae;
+    }
+
+`;
+
+export const SearchIcon = style.i`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    font-size: 30px;
+`;
--- School-ADM/src/pages/Students.tsx
+++ School-ADM/src/pages/Students.tsx
@@ -1,17 +1,23 @@
-
+import { SearchBar, SearchField, SearchIcon, IconContainer } from "./StudentStyle";
 import LateralMenu from "../layouts/LateralMenu";
 import { ContentArea } from "./FlexController";
 import { ContentContainer } from "./HomeStyle";
 
 function Students(){
+
+    const title = 'aluno';
     
     return(
     <>
         <ContentArea>
             <LateralMenu />
             <ContentContainer>
-                <h1> teste </h1>
-
+                <SearchBar> 
+                    <SearchField type='text' placeholder={`buscar ${title}`} />
+                    <IconContainer>
+                        <SearchIcon className='fab fa-instagram'/>
+                    </IconContainer>  
+                </SearchBar>
             </ContentContainer>
         </ContentArea>
        
