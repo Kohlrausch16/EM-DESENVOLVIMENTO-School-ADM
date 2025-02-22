88c7c83f1963abd865d8baeddbc993c9b9cbd5a8
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-02-22 19:33:11 -0300

    implementação projeto spring

---
+++ Desktop/School-ADM/Backend/src/main/java/com/personalproject/schooADM/SchooAdmApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.schooADM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooAdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooAdmApplication.class, args);
+	}
+
+}
---
+++ Desktop/School-ADM/Backend/src/test/java/com/personalproject/schooADM/SchooAdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.schooADM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooAdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
