d05f725bbe3a17804113a74c37b4213f2d1b8354
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-02-23 10:34:12 -0300

    criação classe Language

---
+++ Desktop/School-ADM/Backend/src/main/java/com/personalproject/schooADM/entities/Language.java
@@ -0,0 +1,61 @@
+package com.personalproject.schooADM.entities;
+
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class Language {
+
+    private String id;
+    private String flagIcon;
+    private String roundFlagIcon;
+    private Boolean activeStatus;
+
+
+    public Language(String flagIcon, String roundFlagIcon, Boolean activeStatus) {
+        this.flagIcon = flagIcon;
+        this.roundFlagIcon = roundFlagIcon;
+        this.activeStatus = activeStatus;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFlagIcon() {
+        return flagIcon;
+    }
+
+    public void setFlagIcon(String flagIcon) {
+        this.flagIcon = flagIcon;
+    }
+
+    public String getRoundFlagIcon() {
+        return roundFlagIcon;
+    }
+
+    public void setRoundFlagIcon(String roundFlagIcon) {
+        this.roundFlagIcon = roundFlagIcon;
+    }
+
+    public Boolean getActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(Boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Language language = (Language) o;
+        return Objects.equals(getId(), language.getId()) && Objects.equals(getActiveStatus(), language.getActiveStatus());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getActiveStatus());
+    }
+}
