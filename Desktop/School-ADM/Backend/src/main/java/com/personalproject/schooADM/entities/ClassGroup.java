f14f2e61d5dc5f60ef1134ccadfb18cab92a1c0a
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-03-15 23:56:40 -0300

    inicialização classe ClassGroup

--- Desktop/School-ADM/Backend/src/main/java/com/personalproject/schooADM/entities/ClassGroup.java
+++ Desktop/School-ADM/Backend/src/main/java/com/personalproject/schooADM/entities/ClassGroup.java
@@ -1,4 +1,22 @@
 package com.personalproject.schooADM.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "classGroup")
 public class ClassGroup {
+
+    private String id;
+
+    private String name;
+
+    private Course course;
+
+    private Teacher teacher;
+
+    private List<Student> studentList = new ArrayList<>();
+
 }
